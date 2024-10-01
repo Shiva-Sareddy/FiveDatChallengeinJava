8d59f44091bcc9ffb42e1b7f92c5726d0eef8a9e
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-10-01 21:12:56 +0530

    first commit

---
+++ FiveDayChallenge/Day02Tasks/com/FDC/Day02/Question1.java
@@ -0,0 +1,30 @@
+package com.FDC.Day02;
+
+import java.util.Scanner;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Word to Check Palindrome or not: ");
+		String word = sc.nextLine().toLowerCase();
+		int n = word.length();
+		boolean isPalindrome = true;
+		
+		for(int i = 0; i < n/2 ; i++) {
+			if(word.charAt(i) != word.charAt(n-1-i)) {
+				isPalindrome = false;
+				break;
+			}
+		}
+		
+		if(isPalindrome) {
+			System.out.println("Palindrome");
+		}else {
+			System.out.println("Not a Palindrome");
+		}
+		sc.close();
+
+	}
+
+}
---
+++ FiveDayChallenge/Day02Tasks/com/FDC/Day02/Question2.java
@@ -0,0 +1,20 @@
+package com.FDC.Day02;
+
+import java.util.Scanner;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a Number: ");
+		int n = sc.nextInt();
+		int product = 1;
+		for(int i = 1; i <= n; i++) {
+			product = product * i;
+		}
+		System.out.println("The Factorial of " + n + " is: " + product);
+		sc.close();
+
+	}
+
+}
