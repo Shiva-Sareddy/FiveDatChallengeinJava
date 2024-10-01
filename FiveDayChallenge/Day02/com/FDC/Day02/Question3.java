7f6945ce1b1d00039fb9040cfedd8da1066291a0
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-10-01 23:00:39 +0530

    first commit

--- FiveDayChallenge/Day02/com/FDC/Day02/Question3.java
+++ FiveDayChallenge/Day02/com/FDC/Day02/Question3.java
@@ -1,9 +1,18 @@
 package com.FDC.Day02;
 
+import java.util.Scanner;
+
 public class Question3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Length: ");
+		int length = sc.nextInt();
+		System.out.print("Enter Breadth: ");
+		int breadth = sc.nextInt();
+		String res = (length == breadth) ? "Square" : "Rectangle";
+		System.out.println(res);
+		sc.close();
 
 	}
 
