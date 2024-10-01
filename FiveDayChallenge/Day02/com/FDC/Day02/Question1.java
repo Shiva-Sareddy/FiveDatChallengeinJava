390efb069cfacef033dcea5d81ffcc6cfae6aaef
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-10-01 22:19:45 +0530

    second commit

--- FiveDayChallenge/Day02/com/FDC/Day02/Question1.java
+++ FiveDayChallenge/Day02/com/FDC/Day02/Question1.java
@@ -1,10 +1,36 @@
 package com.FDC.Day02;
 
+import java.util.Scanner;
+
 public class Question1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Number: ");
+		int a = sc.nextInt();
+		System.out.print("Enter Second Number: ");
+		int b = sc.nextInt();
+		System.out.print("Enter Third Number: ");
+		int c = sc.nextInt();
+		
+		System.out.print("The Greatest Number is: ");
+		
+		if(a > b) {
+			if(a > c) {
+				System.out.println(a);
+			}else {
+				System.out.println(c);
+			}
+		}else if(b > a) {
+			if(b > c) {
+				System.out.println(b);
+			}else {
+				System.out.println(c);
+			}
+		}else {
+			System.out.println(a);
+		}
+		sc.close();
 	}
 
 }
